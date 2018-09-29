918c7754526e4bc085c1a357cbb0fa6b36d7b4d4
Davnit <davnit31@gmail.com>
2018-09-29 09:41:01 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='capidaptor',
+    version='0.1',
+    description='Proxy to allow legacy BNET clients to connect to the chat API',
+    author='Davnit',
+    author_email='david@davnit.net',
+    install_requires=['websocket-client']
+)
