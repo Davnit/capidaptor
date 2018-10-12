b8d93c038ed9ebf42908738feeca899311ab6558
Davnit <davnit31@gmail.com>
2018-10-12 15:27:20 -0400

    Update version number to 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='capidaptor',
-    version='0.1',
+    version='1.0',
     description='Proxy to allow legacy BNET clients to connect to the chat API',
     author='Davnit',
     author_email='david@davnit.net',
